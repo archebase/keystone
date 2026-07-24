d94a4fe25f74fd387b1b4826ca9a4c3ffdd9be23
chaoliu <mandoreshark@gmail.com>
2026-07-24 11:06:10 +0800

    chore(swift): remove extracted client SDK

--- sdk/swift/Tests/DGWProtoTests/PlaceholderTests.swift
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2026 ArcheBase
-// SPDX-License-Identifier: MulanPSL-2.0
-
-import Testing
-@testable import DGWProto
-
-@Test func dgwProtoModuleNameIsStable() {
-    #expect(DGWProtoModule.name == "DGWProto")
-}
